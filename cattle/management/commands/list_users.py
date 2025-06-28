a1db8f349ef6bd9cf4c9926fa15ba6a5dc110e40
NT <rm0135ri@gmail.com>
2025-06-28 22:26:31 +0900

    Set is_staff=True for all users

--- cattle/management/commands/list_users.py
+++ cattle/management/commands/list_users.py
@@ -2,9 +2,14 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
 class Command(BaseCommand):
-    help = 'ユーザー一覧を表示'
+    help = '全ユーザーにis_staff=Trueを付与'
 
     def handle(self, *args, **options):
         User = get_user_model()
         for user in User.objects.all():
-            self.stdout.write(f'username: {user.username}, email: {user.email}, is_superuser: {user.is_superuser}') 
+            if not user.is_staff:
+                user.is_staff = True
+                user.save()
+                self.stdout.write(f'Staff権限を付与: {user.username}')
+            else:
+                self.stdout.write(f'既にStaff: {user.username}') 
